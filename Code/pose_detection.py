bf8372e9f495c9ded7eac9fe8154451abbc64595
BJTECNM <192t0220@itsm.edu.mx>
2025-02-19 14:32:16 -0600

    Pose Detection

---
+++ Code/pose_detection.py
@@ -0,0 +1,32 @@
+import cv2
+import mediapipe as mp
+
+# Inicializar las bibliotecas de MediaPipe y OpenCV
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose()
+
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    
+    if not ret:
+        print("No se pudo capturar la imagen")
+        break
+
+    # Convertir la imagen a RGB
+    image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = pose.process(image_rgb)
+
+    if results.pose_landmarks:
+        mp_drawing.draw_landmarks(frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
+    # Mostrar el frame con las marcas de la postura
+    cv2.imshow('Mediapipe Pose Detection', frame)
+
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
